b1ba74b095de7b83e7b07f9102251cd2f2ede15a
lin <shilinxi@proton.me>
2023-11-11 21:34:10 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const uuid = require('uuid');
 app.use(bodyParser.json());
 
 
-let users = [];
-
+let users = [
+  {
+    id: 1,
+    name: 'Dan',
+    favouriteMovie: ["The Fountain"]//has to be an array
+  }
+];
 let movies = [];
 
-
+//LOCAL STORAGE
 let topMovies = [
   {
     title: 'There Will Be Blood',
@@ -28,12 +33,12 @@ let topMovies = [
     year:'2000'
   },
   {
-    title: 'Spirited Away ',
+    title: 'Spirited Away',
     director:'Hayao Miyazaki',
     year:'2001'
   },
   {
-    title: '24 Hour Party People ',
+    title: '24 Hour Party People',
     director:'Michael Winterbottom ',
     year:'2002'
   },
@@ -43,7 +48,7 @@ let topMovies = [
     year:'2005'
   },
   {
-    title: 'Dogtooth ',
+    title: 'Dogtooth',
     director:'Yorgos Lanthimos',
     year:'2009'
   },
@@ -58,7 +63,7 @@ let topMovies = [
     year:'2007'
   },
   {
-    title: 'Once Upon a Time in Hollywood ',
+    title: 'Once Upon a Time in Hollywood',
     director:'Quentin Tarantino',
     year:'2019'
   }
@@ -66,11 +71,124 @@ let topMovies = [
 
 //http get method, take in a string doe endpoint, a function includes a request and a response
 app.get ('/', (req, res) => {
-  res.send('Top moveis of the 21st century!');
+  res.send('Hello, this is the font page of the movie app, which has not been built yet..');
+});
+
+app.get('/top10',(req, res)=> {
+  //res.send('Top moveis of the 21st century!');
+  res.status(200).json(topMovies);
+});
+//checking if the code works with top 10 movies array
+app.get('/top10/:movieName',(req, res)=> {
+  const { movieName } = req.params;
+  const movie = topMovies.find( movie => movie.title === movieName)
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(404).send('Sorry, no movies found');
+  }
 });
 
 app.get('/movies',(req, res) => {
-  res.json(topMovies);
+  res.status(200).send('Successful GET request retuning data on all movies');
+});
+
+app.get('/movies/:movieName', (req, res)=>{
+  const { movieName } = req.params;
+  const movie = movies.find( movie => movie.title === movieName)
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(404).send('Sorry, no movies found');
+  }
+});
+
+app.get('/movies/genre/:genreName', (req, res)=> {
+  const { genreName } = req.params;
+  const genre = movies.find( movie => movie.genre.name === genreName)// access the property of the object
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(404).send('Sorry, no genre found');
+  }
+});
+
+app.get('/movies/directors/:directorName', (req, res)=> {
+  const { directorName } = req.params;
+  const director = movies.find( movie => movie.director.name === directorName)
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(404).send('Sorry, no directors found');
+  }
+});
+//create
+app.post('/users/register', (req, res)=> {
+  const newUser = req.body;
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('users need names')
+  }
+});
+
+//update
+app.put('/users/:userId/profile', (req, res)=> {
+  const { userId } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == userId); //use two equal signs because the uuid is a number but user input will be a string
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user).send('movie added');
+  } else {
+    res.status(400).send('Sorry, no users found');
+  }
+});
+
+app.post('/users/:userId/myfavourites', (req, res)=> {
+  const { userId } = req.params;
+  const movieTitle = req.body.favouriteMovie;
+
+  let user = users.find( user => user.id == userId);
+  if (user) {
+    user.favouriteMovie.push(movieTitle);
+    res.status(200).send(`${movieTitle} is added to My Favourites`);
+  } else {
+    res.status(400).send('Sorry, no users found');
+  }
+});
+
+app.delete('/users/:userId/myfavourites', (req, res)=> {
+  const { userId } = req.params;
+  const movieTitle = req.body.favouriteMovie;
+
+  let user = users.find( user => user.id == userId);
+  if (user) {
+    user.favouriteMovie = user.favouriteMovie.filter( title => title !== movieTitle);//filter out all movies that is not movieTitle
+    res.status(200).send(`${movieTitle} is removed from your list!`);
+  } else {
+    res.status(400).send('Sorry, no users found');
+  }
+});
+
+app.delete('/users/:userId/account-setting', (req, res)=> {
+  const { userId } = req.params;
+
+  let user = users.find( user => user.id == userId);
+  if (user) {
+    users = users.filter( user => user.id != userId);
+    res.status(200).send(`${userId} is deleted!`);
+  } else {
+    res.status(400).send('Sorry, no users found');
+  }
 });
 
 app.use(express.static('public'));
