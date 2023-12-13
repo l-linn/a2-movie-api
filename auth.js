22bab01bd8a66fa9ada4517bff8870117d8207e5
lin <shilinxi@proton.me>
2023-12-13 21:14:20 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,6 +6,9 @@ const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the 
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
+const Models = require('./models.js');
+const Users = Models.user;
+
 require('./passport.js'); // Your local passport file
 
 let generateJWTToken = (user) => {
@@ -19,7 +22,7 @@ let generateJWTToken = (user) => {
 //POST LOGIN
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    let hashedPassword = users.hashPassword(req.body.password);
+    let hashedPassword = Users.hashPassword(req.body.password);
     passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
