8fa92a04d546d379bb15a343bd52082d2a066e90
lin <lin7cb@outlook.com>
2024-01-16 12:22:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose.connect('mongodb+srv://lin7cb:c3lJ78n0A23nLqFp@cluster0.vkzg8pg.mongodb
 
 
 //create new user
-app.post('/users/register', [
+app.post('/register', [
   check('username', 'Username is required').isLength({min: 5}),
   check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('password', 'Password is required').not().isEmpty(),
